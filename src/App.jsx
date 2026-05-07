5a66a785e5c7656aa5bb2c1d856cd5b9171f7534
mrzack <mohdzakariya2300@gmail.com>
2026-05-07 17:51:41 +0530

    stop both form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
 // import Login from "./loginForm/Login";
-import Signup from "./loginForm/Signup";
+// import Signup from "./loginForm/Signup";
 const App = ()=>{
   return(
 <div>
   {/* <Login></Login> */}
-  <Signup></Signup>
+  {/* <Signup></Signup> */}
     </div>
   )
 }
